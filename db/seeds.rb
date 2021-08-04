a4ee8069be3e9300a3a58d5b9a56607ed6fc3cea
Dani Coleman <danicoleman00@gmail.com>
2021-08-04 17:26:26 -0600

    Create user seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,4 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+user = User.create(first_name: 'Dani', last_name: 'Coleman', email: 'dani.coach@mobile.edu', password: '1234', cell_phone: '', signature: '', internal_email: 'dani.mycollege@localhost:3001', token: SecureRandom.hex)
