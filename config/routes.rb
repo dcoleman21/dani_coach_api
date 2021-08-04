9fbb737f225149d9fcbe79799484f55296bce585
Dani Coleman <danicoleman00@gmail.com>
2021-08-04 17:43:47 -0600

    Add /login to my routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  resource :users, only: [:create]
-  # used for logging in
-  post "/login", to: "users#login"
-  # verify logged in using JWT
-  get "/auto_login", to: "user#auto_login"
+  namespace :api do
+    namespace :v0 do
+      post "/login", to: "users#login"
+    end
+  end
 end
