4716d58dd19ac7df53d072e9961f0de98dc4c828
Dani Coleman <danicoleman00@gmail.com>
2021-08-04 17:56:12 -0600

    Add validations for added user attributes

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,13 @@
 class User < ApplicationRecord
   has_secure_password
+  has_secure_token :token
 
+  validates :first_name, presence: true
+  validates :last_name, presence: true
   validates :email, uniqueness: true, presence: true
+  validates :cell_phone, presence: true
+  validates :signature, presence: true
+  validates :internal_email, presence: true
   validates :password, presence: true
+  validates :token, uniqueness: true, presence: true
 end
