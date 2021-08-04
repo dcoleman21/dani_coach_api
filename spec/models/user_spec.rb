078a075a767cd577a1d16f656bb09536a7bddc7a
Dani Coleman <danicoleman00@gmail.com>
2021-08-04 17:56:33 -0600

    Add validation tests for added user attributes

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,7 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  it { should validate_presence_of :email }
-  it { should validate_uniqueness_of :email }
-  it { should validate_presence_of :password }
+  describe 'validations' do
+    it { should validate_presence_of :first_name }
+    it { should validate_presence_of :last_name }
+    it { should validate_presence_of :email }
+    it { should validate_uniqueness_of :email }
+    it { should validate_presence_of :cell_phone }
+    it { should validate_presence_of :signature }
+    it { should validate_presence_of :internal_email }
+    it { should validate_presence_of :password }
+    it { should validate_presence_of :token }
+    it { should validate_uniqueness_of :token }
+  end
 end
