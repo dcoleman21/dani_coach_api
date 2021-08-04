ae696d8384f228486bca4b1b620031c07a3a1c57
Dani Coleman <danicoleman00@gmail.com>
2021-08-04 11:56:40 -0600

    Add resource for :users

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resource :users, only: [:create]
+  # used for logging in
+  post "/login", to: "users#login"
+  # verify logged in using JWT
+  get "/auto_login", to: "user#auto_login"
 end
