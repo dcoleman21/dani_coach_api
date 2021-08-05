e8da6e9d002c57f6836e0181f7ab0b52025c0476
Dani Coleman <danicoleman00@gmail.com>
2021-08-05 15:50:58 -0600

    Add join table for notes

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_05_200456) do
+ActiveRecord::Schema.define(version: 2021_08_05_214939) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -26,6 +26,16 @@ ActiveRecord::Schema.define(version: 2021_08_05_200456) do
     t.index ["player_id"], name: "index_assessments_on_player_id"
     t.index ["tournament_id"], name: "index_assessments_on_tournament_id"
     t.index ["user_id"], name: "index_assessments_on_user_id"
+  end
+
+  create_table "notes", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.string "note"
+    t.bigint "assessment_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["assessment_id"], name: "index_notes_on_assessment_id"
+    t.index ["user_id"], name: "index_notes_on_user_id"
   end
 
   create_table "players", force: :cascade do |t|
@@ -70,4 +80,6 @@ ActiveRecord::Schema.define(version: 2021_08_05_200456) do
   add_foreign_key "assessments", "players"
   add_foreign_key "assessments", "tournaments"
   add_foreign_key "assessments", "users"
+  add_foreign_key "notes", "assessments"
+  add_foreign_key "notes", "users"
 end
