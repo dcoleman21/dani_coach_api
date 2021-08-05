48eeb45c50f1cd2629765569f6dbf7ca9178b717
Dani Coleman <danicoleman00@gmail.com>
2021-08-05 12:52:23 -0600

    Add seed data for tournament and team

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,3 @@
-require 'faker'
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
 #
@@ -6,12 +5,10 @@ require 'faker'
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-@tournament = FactoryBot.create(:tournament)
-5.times do
-  FactoryBot.create(tournament: @tournament)
-end
+@tournament1 = Tournament.create!(name: "The Orange Classic", city: "Orlando", state: "FL", start_date: "2021-09-05")
+@tournament2 = Tournament.create!(name: "The Turkey Classic", city: "Cleveland", state: "OH", start_date: "2021-11-05")
+@tournament3 = Tournament.create!(name: "High School All American Showcase Fall", city: "Denver", state: "CO", start_date: "2021-10-05")
 
-@team = FactoryBot.create(:team)
-5.time do
-  FactoryBot.create(team: @team)
-end
+@team1 = Team.create!(name: "Orlando Blaze", age_group: "N/A")
+@team2 = Team.create!(name: "Ohio All Stars", age_group: "N/A")
+@team2 = Team.create!(name: "Mile High Magic", age_group: "N/A")
