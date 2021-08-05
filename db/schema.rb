9dcd3d7658a4f17f2fcd057e1e4d39d94f3240d6
Dani Coleman <danicoleman00@gmail.com>
2021-08-05 13:13:37 -0600

    Add players table

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,23 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_05_185428) do
+ActiveRecord::Schema.define(version: 2021_08_05_191231) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "players", force: :cascade do |t|
+    t.string "first_name"
+    t.string "last_name"
+    t.integer "height"
+    t.integer "weight"
+    t.string "birthday"
+    t.integer "graduation_year"
+    t.string "position"
+    t.boolean "recruit"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "teams", force: :cascade do |t|
     t.string "name"
