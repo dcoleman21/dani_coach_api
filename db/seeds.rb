75eedca9d8b453ed78fb92ec2e29ad0d30882127
Dani Coleman <danicoleman00@gmail.com>
2021-08-05 12:24:02 -0600

    Add team seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,4 @@
+require 'faker'
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
 #
@@ -8,4 +9,9 @@
 @tournament = FactoryBot.create(:tournament)
 5.times do
   FactoryBot.create(tournament: @tournament)
+end
+
+@team = FactoryBot.create(:team)
+5.time do
+  FactoryBot.create(team: @team)
 end
