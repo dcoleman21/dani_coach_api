4664dc9e63a7449b8f23b661dc3f661921a35473
Dani Coleman <danicoleman00@gmail.com>
2021-08-05 12:11:58 -0600

    Add teams table

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_05_152804) do
+ActiveRecord::Schema.define(version: 2021_08_05_175724) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "teams", force: :cascade do |t|
+    t.string "name"
+    t.string "age_group"
+    t.string "coach"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "tournaments", force: :cascade do |t|
     t.string "name"
