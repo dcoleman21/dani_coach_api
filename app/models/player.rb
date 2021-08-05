087835ea5fefa99a5e0a474ebd0885aab7aefa2c
Dani Coleman <danicoleman00@gmail.com>
2021-08-05 17:00:06 -0600

    Update player model birthday format

--- app/models/player.rb
+++ app/models/player.rb
@@ -3,8 +3,8 @@ class Player < ApplicationRecord
   validates :last_name, presence: true
   validates :height, presence: true
   validates :weight, presence: true
-  validates :birthday, presence: true
-  validates :graduation_year, format: { with: /^\d{4}-\d{2}-\d{2}$/, multiline: true }, presence: true
+  validates :birthday, format: { with: /^\d{4}-\d{2}-\d{2}$/, multiline: true }, presence: true
+  validates :graduation_year, presence: true
   validates :position, presence: true
   validates :recruit, presence: true
 end
