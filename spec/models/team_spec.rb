5f9b1d54eeabe1fc5c00cd98c63802badc0b8d59
Dani Coleman <danicoleman00@gmail.com>
2021-08-05 12:51:10 -0600

    Comment out coach attribute

--- spec/models/team_spec.rb
+++ spec/models/team_spec.rb
@@ -4,6 +4,6 @@ RSpec.describe Team, type: :model do
   describe 'validations' do
     it { should validate_presence_of :name }
     it { should validate_presence_of :age_group }
-    it { should validate_presence_of :coach }
+    # it { should validate_presence_of :coach }
   end
 end
