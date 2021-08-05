4455ea852dd7472cae4951d995fc418cf980c582
Dani Coleman <danicoleman00@gmail.com>
2021-08-05 12:51:31 -0600

    Comment out coach attribute

--- app/models/team.rb
+++ app/models/team.rb
@@ -1,5 +1,5 @@
 class Team < ApplicationRecord
   validates :name, presence: true
   validates :age_group, presence: true
-  validates :coach, presence: true
+  # validates :coach, presence: true
 end
