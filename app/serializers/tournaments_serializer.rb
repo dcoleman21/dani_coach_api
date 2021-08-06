f6ff5319d2693cebc21b94560280b2045bd8fef8
Dani Coleman <danicoleman00@gmail.com>
2021-08-06 11:51:55 -0600

    Add set_type to tournaments serializer

--- app/serializers/tournaments_serializer.rb
+++ app/serializers/tournaments_serializer.rb
@@ -1,5 +1,6 @@
 class TournamentsSerializer
   include FastJsonapi::ObjectSerializer
+  set_type 'tournaments'
   attributes :name, :city, :state, :start_date
   has_many :teams
   has_many :players, through: :teams
