105794d46d4028ceb1771b7c7779672b293b2049
Dani Coleman <danicoleman00@gmail.com>
2021-08-06 14:37:07 -0600

    Add options block to add teams when rendering tournaments

--- app/controllers/api/v0/tournaments_controller.rb
+++ app/controllers/api/v0/tournaments_controller.rb
@@ -1,6 +1,13 @@
 class Api::V0::TournamentsController < ApplicationController
   def index
     tournaments = Tournament.all
-    render json: TournamentsSerializer.new(tournaments)
+    render json: TournamentSerializer.new(tournaments)
+  end
+
+  def show
+    tournament = Tournament.find(params[:id])
+    options = {}
+    options[:include] = [:teams]
+    render json: TournamentSerializer.new(tournament, options)
   end
 end
