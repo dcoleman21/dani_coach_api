4162e83adce029437c5397aca6f705a966d7454a
Dani Coleman <danicoleman00@gmail.com>
2021-08-06 11:06:43 -0600

    Add relationships

--- spec/models/team_spec.rb
+++ spec/models/team_spec.rb
@@ -1,6 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe Team, type: :model do
+  describe 'relationships' do
+    it { should have_many :players }
+    it { should have_many :tournament_teams }
+    it { should have_many(:tournaments).through(:tournament_teams) }
+  end
   describe 'validations' do
     it { should validate_presence_of :name }
     it { should validate_presence_of :age_group }
