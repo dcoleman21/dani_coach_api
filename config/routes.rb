834e388ac4f1f9205cd0e57027d6b7867d18cfcd
Dani Coleman <danicoleman00@gmail.com>
2021-08-06 16:11:15 -0600

    Nest resources teams within resources tournaments

--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,15 @@ Rails.application.routes.draw do
     namespace :v0 do
       # resource :users, only: [:create]
       # post "/login", to: "users#login"
-      # resources :tournaments
-      get "/tournaments", to: "tournaments#index"
-      get "/tournaments/:id", to: "tournaments#show"
+      resources :tournaments, only: [:index, :show] do
+        resources :teams, only: [:index]
+      end
+      # story #4
+      # api/v0/players/id/assessments
+      # resources :players do
+      #   #assessment controller
+      #   resources :assessments, only: [:index]
+      # end
     end
   end
 end
