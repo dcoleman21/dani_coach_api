2240d8e1a83375819f8733cc1972254fae44b2bf
Dani Coleman <danicoleman00@gmail.com>
2021-08-06 12:01:56 -0600

    Add describe block

--- spec/models/tournament_team_spec.rb
+++ spec/models/tournament_team_spec.rb
@@ -1,6 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe TournamentTeam, type: :model do
-  it { should belong_to :team }
-  it { should belong_to :tournament }
+  describe 'relationships' do
+    it { should belong_to :team }
+    it { should belong_to :tournament }
+  end
 end
