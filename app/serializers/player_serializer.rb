583278fa5488ab9a395c0b7066ff532193e7fdf7
Dani Coleman <danicoleman00@gmail.com>
2021-08-06 16:31:20 -0600

    Create player serializer

---
+++ app/serializers/player_serializer.rb
@@ -0,0 +1,13 @@
+class PlayerSerializer
+  include FastJsonapi::ObjectSerializer
+  set_type 'players'
+  attributes :first_name,
+             :last_name,
+             :height,
+             :weight,
+             :birthday,
+             :graduation_year,
+             :position,
+             :recruit
+
+end
