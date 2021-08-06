ba3d716ae13d367b47291162e2fc6a4db4f83293
Dani Coleman <danicoleman00@gmail.com>
2021-08-06 11:09:48 -0600

    Add relationship test

--- spec/models/player_spec.rb
+++ spec/models/player_spec.rb
@@ -1,6 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe Player, type: :model do
+  describe 'relationships' do
+    it { should belong_to :team }
+  end
   describe 'validations' do
     it { should validate_presence_of :first_name }
     it { should validate_presence_of :last_name }
