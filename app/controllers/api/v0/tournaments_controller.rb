7c9c9ef7b98fb158bb67048fcab6bba9a5c656e2
Dani Coleman <danicoleman00@gmail.com>
2021-08-06 11:12:11 -0600

    Render tournaments serializer to return expected json response

--- app/controllers/api/v0/tournaments_controller.rb
+++ app/controllers/api/v0/tournaments_controller.rb
@@ -1,5 +1,6 @@
 class Api::V0::TournamentsController < ApplicationController
   def index
-    render json: TournamentSerializer.new
+    tournaments = Tournament.all
+    render json: TournamentsSerializer.new(tournaments)
   end
 end
