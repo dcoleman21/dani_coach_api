09d6593331254b41c4c350fe167789e2ca7bb9e4
Dani Coleman <danicoleman00@gmail.com>
2021-08-07 16:13:05 -0600

    Comment out controller for now. WIP

--- app/controllers/api/v0/assessments_controller.rb
+++ app/controllers/api/v0/assessments_controller.rb
@@ -1,11 +1,11 @@
 class Api::V0::AssessmentsController < ApplicationController
-  def create
-    render json: AssessmentSerializer.new(Assessment.create(params[:assessment]))
-  end
-
-  private
-
-  def assessment_params
-    params.require(:assessment).permit(:rating, :player_id, :tournament_id, :assessment_type)
-  end
+  # def create
+  #   render json: AssessmentSerializer.new(Assessment.create(params[:assessment]))
+  # end
+  #
+  # private
+  #
+  # def assessment_params
+  #   params.require(:assessment).permit(:rating, :player_id, :tournament_id, :assessment_type)
+  # end
 end
