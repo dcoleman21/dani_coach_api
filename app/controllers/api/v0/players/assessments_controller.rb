4e46495a7b81aa5fe95bad5c1916cacadbdfd196
Dani Coleman <danicoleman00@gmail.com>
2021-08-07 16:12:45 -0600

    Comment out controller for now. WIP

---
+++ app/controllers/api/v0/players/assessments_controller.rb
@@ -0,0 +1,8 @@
+class Api::V0::Players::AssessmentsController < ApplicationController
+  # def index
+  #   assessment = Assessment.find(params[:id])
+  #   options = {}
+  #   options[:include] = [:players]
+  #   render json: PlayerSerializer.new(assessment, options)
+  # end
+end
