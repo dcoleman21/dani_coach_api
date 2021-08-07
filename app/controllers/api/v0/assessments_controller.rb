0e43a3aed50bcf3714be962a8645e2898175379c
Dani Coleman <danicoleman00@gmail.com>
2021-08-07 15:55:40 -0600

    Add strong params for assessment create action. Not creating

--- app/controllers/api/v0/assessments_controller.rb
+++ app/controllers/api/v0/assessments_controller.rb
@@ -1,5 +1,11 @@
 class Api::V0::AssessmentsController < ApplicationController
   def create
-    render json: AssessmentSerializer.new(Assessment.first)
+    render json: AssessmentSerializer.new(Assessment.create(params[:assessment]))
+  end
+
+  private
+
+  def assessment_params
+    params.require(:assessment).permit(:rating, :player_id, :tournament_id, :assessment_type)
   end
 end
