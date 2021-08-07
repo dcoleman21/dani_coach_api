b0003fbe8eaa0006a2c2139b87c87fc929594e9a
Dani Coleman <danicoleman00@gmail.com>
2021-08-07 16:11:28 -0600

    Add a player namespace

--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,9 @@ Rails.application.routes.draw do
       # story #4
       # api/v0/assessments?included=players
       resources :assessments, only: [:create]
-
+      namespace :players do
+        get "/:id", to: "assessments#index"
+      end
       # resources :players
     end
   end
